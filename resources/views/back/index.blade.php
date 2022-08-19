cae568c4e222bb862cae9236dea7eb2fe9e090ee
Amirhossein-Shahbaz <shahbazamirhossein2001@gmail.com>
2022-08-19 20:39:43 +0430

    Add beautiful text editor

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -25,6 +25,22 @@
     <link rel="stylesheet" href="{{ asset('/back/assets/css/demo_1/style.css') }}">
     <!-- End Layout styles -->
     <link rel="shortcut icon" href="{{ asset('/back/assets/images/favicon.png') }}" />
+
+
+    <script src="https://cdn.tiny.cloud/1/xwizw2s2xfcngylt0xztupsxfiepfwejt8pnrfy0xjqhm1cb/tinymce/6/tinymce.min.js"
+        referrerpolicy="origin"></script>
+
+
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            plugins: 'a11ychecker advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinycomments tinymcespellchecker',
+            toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter image editimage pageembed permanentpen table tableofcontents',
+            toolbar_mode: 'floating',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Author name',
+        });
+    </script>
 </head>
 
 <body>
